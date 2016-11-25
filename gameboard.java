65777c4c7ff0a71fcd6cb7def0714e3e6d836a3d
ShainaR <S1371180@student.mcckc.edu>
2016-11-25 15:08:20 -0600

    Create gameboard.java

---
+++ gameboard.java
@@ -0,0 +1,11 @@
+package prototypeBattleshipGame;
+
+public class gameboard {
+	
+	int[][] beginner = new int[6][6];
+	int[][] standard = new int[9][9];
+	int[][] advanced = new int[12][12];
+	
+	
+
+}
