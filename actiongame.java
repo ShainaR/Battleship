7532f87b1f0fd6fe54ab2bf65b0d85324e644d68
ShainaR <S1371180@student.mcckc.edu>
2016-12-05 15:58:57 -0600

    Update actiongame.java

--- actiongame.java
+++ actiongame.java
@@ -1,1 +1,21 @@
+package battleshipgame;
 
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class actiongame {
+	Gameboard gameboard1 = new Gameboard();
+	Scanner input = new Scanner(System.in);
+	
+	public void startGame(){
+		
+		gameboard1.difficultyInfo();
+		gameboard1.createShipArray();
+		gameboard1.fillBoard();
+		gameboard1.positionShips();
+		gameboard1.outputGame();
+		gameboard1.endGame();
+	}
+	
+	
+}
