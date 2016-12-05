432d7490577f57a49df529fc881a62395bb0feb0
ShainaR <S1371180@student.mcckc.edu>
2016-12-05 15:59:59 -0600

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -1,25 +1,15 @@
 package battleshipgame;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
 
 
 public class battleship {
 	
-	//private static final char[][] ShipBoard = null;
-	static Gameboard gameboard1;
 	public static void main(String[] args) {
-		gameboard1 = new Gameboard();
 		
-		//gameboard1.difficultyInfo();
-		// gameboard1.fillGameBoard();
-		gameboard1.outputGame();
-		//gameboard1.outputGame();
-		// gameboard1.setShips(location);	
-
-		// gameboard1.setShipInfo();
-		
+		actiongame playGame = new actiongame();
+		playGame.startGame();
 		
 	}
 
 }
+				
+	
