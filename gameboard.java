d333173be98bb9d7b0ce2622705e4a8d5727ec54
ShainaR <S1371180@student.mcckc.edu>
2016-12-07 10:42:34 -0600

    Update gameboard.java

--- gameboard.java
+++ gameboard.java
@@ -46,15 +46,17 @@ public class Gameboard {
 		char miss = '*';
 		char hit = 'X';
 		
+		
 		System.out.print("   ");
+		// Print letters on top of board
 		for (int i = 0; i < gameBoard[0].length; i++) {
 			System.out.printf("%3d", i+1);
-			System.out.print("  ");
 
 		}
 		
 		System.out.println();
-		//loop through array's rows
+		
+		//loop through gameBoard's rows
 		for (int row = 0; row < gameBoard[0].length; row++)
 		{	
 			System.out.printf("%3c", getLetter(row+1));
@@ -68,9 +70,9 @@ public class Gameboard {
 						System.out.printf("%3c", miss);
 					if (gameBoard[row][column] == 2)
 						System.out.printf("%3c", hit);
-					System.out.printf("%3c", gameBoard[row][column]);
-					
-				//System.out.print(gameBoard[row][column]);
+					if (gameBoard[row][column] == 'A' || gameBoard[row][column] == 'B' || gameBoard[row][column] == 'D' || gameBoard[row][column] == 'S' || gameBoard[row][column] == 'P')
+					System.out.printf("%3c", water);
+	
 			}
 	
 			System.out.println();
@@ -78,6 +80,7 @@ public class Gameboard {
 		}
 	}
 	
+	// method to track hits, score and end game 
 	public void endGame() {		
 		
 		System.out.println(availableMissiles);
@@ -85,19 +88,40 @@ public class Gameboard {
 		while (availableMissiles != 0 && hits != 17) {
 			shotsFired++;
 			
-			System.out.println("Choose the coordinate to attack. Enter the row (LETTER) you wish to attack: ");
-			// convert letter to integer
+			System.out.println("Choose the coordinate to attack. Enter the row you wish to attack. (Ex: A, B, C): ");
+				
 			String guess = input.next();
 			
-			char charInput = guess.charAt(0); 
-
-			int coord1 = getNumber(charInput);
+			// ensure input is upper case
+			guess = guess.toUpperCase();
+			System.out.println(guess);
 			
-					
+			char charInput = guess.charAt(0); 
+			
+			int coord1 = getNumber(charInput);
+			System.out.println(coord1);
+			
+			// ensure coord1 is within boardSize coordinates
+			if (coord1 < 1 || coord1 > boardSize){
+				System.out.println("You did not enter a valid guess.");
+				continue;
+			}
+			
+			// ensure coord1 prints in correct spot
+			coord1 = getNumber(charInput) - 1;
+			
+			// prompt user to enter column coordinates
 			System.out.println("Choose the column you want to attack (NUMBER): ");
+			
+			// ensure coord2 places in correct spot
 			int coord2 = input.nextInt() - 1;
+			if (coord2 > boardSize || coord2 < 0) {
+				System.out.println("You did not enter a valid coordinate.");
+				continue;
+			}
 		
 			
+			// determine which ship was sunk
 			if (gameBoard[coord1][coord2] == 'A') {
 				Aircraft--;
 				if (Aircraft == 0){
@@ -127,6 +151,7 @@ public class Gameboard {
 				}
 			}
 			
+			
 			for (int j = 0; j < shipLengthArray.size(); j++) {
 			// checks to see if it was a hit or a miss
 			for (int i = 0; i < shipLengthArray.get(j); i++) {
@@ -142,12 +167,12 @@ public class Gameboard {
 			if (gameBoard[coord1][coord2] == 0) {
 				//convert to 1 to print * on next board
 				gameBoard[coord1][coord2] = 1;
-				System.out.println("miss");
+				System.out.println("MISS");
 				misses++;
 			} else if (gameBoard[coord1][coord2] == 2) {
 				//prints X on next board
 				
-				System.out.println("hit");
+				System.out.println("HIT");
 				hits++;
 			}
 			
@@ -177,44 +202,86 @@ public class Gameboard {
 		
 	// conversion method to change letters to numbers for int[][] board
 	public int getNumber(char input) {
-		int row = 0;
+		int row = 1;
 		
 		switch(input){
 		case 'A':
-			row = 0;
-			break;
-		case 'B':
 			row = 1;
 			break;
-		case 'C':
+		case 'B':
 			row = 2;
 			break;
-		case 'D':
+		case 'C':
 			row = 3;
 			break;
-		case 'E':
+		case 'D':
 			row = 4;
 			break;
-		case 'F':
+		case 'E':
 			row = 5;
 			break;
-		case 'G':
+		case 'F':
 			row = 6;
 			break;
-		case 'H':
+		case 'G':
 			row = 7;
 			break;
-		case 'I':
+		case 'H':
 			row = 8;
 			break;
-		case 'J':
+		case 'I':
 			row = 9;
 			break;
-		case 'K':
+		case 'J':
 			row = 10;
 			break;
-		case 'L':
+		case 'K':
 			row = 11;
+			break;
+		case 'L':
+			row = 12;
+			break;
+		case 'M' :
+			row = 13;
+			break;
+		case 'N':
+			row = 14;
+			break;
+		case 'O':
+			row = 15;
+			break;
+		case 'P':
+			row = 16;
+			break;
+		case 'Q':
+			row = 17;
+			break;
+		case 'R':
+			row = 18;
+			break;
+		case 'S' :
+			row = 19;
+			break;
+		case 'T':
+			row = 20;
+			break;
+		case 'U':
+			row = 21;
+			break;
+		case 'V':
+			row = 22;
+			break;
+		case 'W':
+			row = 23;
+			break;
+		case 'X':
+			row = 24;
+			break;
+		case 'Y' :
+			row = 25;
+			break;
+		case 'Z' :
+			row = 26;
 			break;
 		
 		}
@@ -357,48 +424,65 @@ public class Gameboard {
 			//loops through arrayLists that hold enum values for shipSize and shipMarker
 			for (int j = 0; j < shipLengthArray.size();j++) {
 				boolean shipsOnBoard = false;
+				
+				boolean spaceFree = false;
 			
 			start:
 			while (!shipsOnBoard){		
 				
 				boolean horizontal = rand.nextBoolean();
+				
 
 				// generate random number for ship placement, either vertical or horizontal starting place
 				int col = rand.nextInt(boardSize);
 				int row = rand.nextInt(boardSize);
 				
-				// ensure starting point of each ship is not taken up
-				if (gameBoard[row][col] != 0)
-					continue start;
+				// ensure row and column are not taken up 
+				// if they are restart while loop
+				if (gameBoard[row][col] > 0) {
+					continue;
+				}
+				
+				// if boolean horizontal is true
 				if (horizontal) {
 					
-					
 					for (int i = 0; i < shipLengthArray.get(j); i++) {
-					gameBoard[row][i+1] = getLetter(shipCharArray.get(j));
-					//if ((i+1) != 0) {
-						//continue;
-					//}
+						System.out.println(gameBoard[row][i+1]);
+						if (gameBoard[row][i+1] != 0) {
+							System.out.println("Searching!");
+							continue start;
+						} else {
+						gameBoard[row][i+1] = getLetter(shipCharArray.get(j));
+						}
 					}
 					
 					
-					
+				// if boolean horizontal is false
 				} else { //vertically
 					
 					for (int i = 0; i < shipLengthArray.get(j); i++) {
+						System.out.println(gameBoard[row][i+1]);
+						if (gameBoard[i+1][col] != 0) {
+							System.out.println("Searching!");
+
+							continue start;
+						} else {
 						gameBoard[i+1][col] = getLetter(shipCharArray.get(j));
-						//if ((i+1) != 0) {
-							//continue;
-						//}
+						}
 					}
 				
 					
-					}
+				}
+				
 				shipsOnBoard = true;
 			
+				//}// end space free
 			} // end if else statement
 				
-		} // end while loop
+				
+		} // end for loop
 			
-		
+			
 	 } // end for loop 
-	}
+
+}
